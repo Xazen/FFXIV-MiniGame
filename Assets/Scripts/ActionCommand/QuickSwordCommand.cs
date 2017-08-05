6fa9f1919e1a705fdf75949aa24bc820bcd8ee0f
Dennis Lieu <dlieu@lieudennis.com>
2017-08-05 23:06:06 +0200

    All PartyMembers do damage

---
+++ Assets/Scripts/ActionCommand/QuickSwordCommand.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class QuickSwordCommand : ActionCommand
+{
+    [SerializeField]
+    private int _attackDamage;
+
+    public override void Execute(Actor target)
+    {
+        base.Execute(target);
+        target.ReduceHp(_attackDamage);
+    }
+}
--- Assets/Scripts/HpGUI.cs
+++ Assets/Scripts/HpGUI.cs
@@ -1,29 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Image))]
-public class FilledImageHpBar : MonoBehaviour
-{
+public class HpGUI : MonoBehaviour {
+
     [SerializeField]
     private Actor _actor;
 
+    [SerializeField]
     private Image _hpBarImage;
-	
-	void Start ()
-    {
-        _hpBarImage = GetComponent<Image>();
 
+    [SerializeField]
+    private Text _hpText;
+
+    void Start()
+    {
         _hpBarImage.fillAmount = _actor.MaxHP;
         _actor.OnHpChangedDelegate += OnHpChanged;
-	}
+
+        _hpText.text = _actor.MaxHP.ToString();
+        _actor.OnHpChangedDelegate += OnHpChanged;
+
+    }
 
     private void OnHpChanged(Actor actor, int oldValue, int newValue)
     {
-        _hpBarImage.fillAmount = (float) newValue / actor.MaxHP;
+        _hpBarImage.fillAmount = (float)newValue / actor.MaxHP;
+        _hpText.text = newValue.ToString();
     }
 
     private void OnDestroy()
     {
+        _actor.OnHpChangedDelegate -= OnHpChanged;
         _actor.OnHpChangedDelegate -= OnHpChanged;
     }
 }
